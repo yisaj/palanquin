5b2797cc3ca9c1d293159427aba9a9dd781f2be1
Sijay Liu <sijay.liu@gmail.com>
2019-06-19 19:57:06 -0700

    Implement bad dots

--- Assets/Scripts/BadDotController.cs
+++ Assets/Scripts/BadDotController.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class BadDotController : MonoBehaviour
 {
+    public float speed = 0.05f;
+    public float carryThrough = 0.1f;
+    public bool left = false;
+    public bool entered = false;
+    public int phase = 0;
+    public float distanceLeft = 0f;
+    public GameController game;
+
+    void Awake()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        if (game.gameIsOver)
+        {
+            return;
+        }
+
+        transform.Translate(new Vector3(speed, 0, 0));
+        if (phase == 2)
+        {
+            distanceLeft += speed;
+            if (distanceLeft > carryThrough)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Collider other = collision.collider;
+        if (other.tag == "Wall")
+        {
+            if (phase == 0)
+            {
+                phase = 1;
+            }
+            else if (phase == 2)
+            {
+                phase = 0;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Collider other = collision.collider;
+        if (other.tag == "Wall" && phase == 1)
+        {
+            phase = 2;
+        }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ public class GameController : MonoBehaviour
 {
     public LevelController level;
     public GoodDotController goodDot;
-    //public BadDotController badDot;
+    public BadDotController badDot;
     public float dotHeight = 0.1f;
     private GoodDotController currentGoodDot;
     [System.NonSerialized]
@@ -14,7 +14,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnBadDot", 2.0f, 5.0f);
     }
 
     // Update is called once per frame
@@ -22,11 +22,11 @@ public class GameController : MonoBehaviour
     {
         if (currentGoodDot == null)
         {
-            currentGoodDot = spawnGoodDot();
+            currentGoodDot = SpawnGoodDot();
         }
     }
 
-    GoodDotController spawnGoodDot()
+    GoodDotController SpawnGoodDot()
     {
         Vector3 position = new Vector3();
         Quaternion rotation = new Quaternion();
@@ -57,6 +57,38 @@ public class GameController : MonoBehaviour
         GoodDotController dot = Instantiate(goodDot, position, rotation);
         dot.game = this;
         return dot;
+    }
+
+    void SpawnBadDot()
+    {
+        Vector3 position = new Vector3();
+        Quaternion rotation = new Quaternion();
+        float halfRange;
+        switch (Random.Range(0, 4))
+        {
+            case 0:
+                halfRange = level.width / 2f - badDot.transform.localScale.x;
+                position = new Vector3(Random.Range(-halfRange, halfRange), dotHeight, level.height / 2f + badDot.carryThrough);
+                rotation = Quaternion.AngleAxis(90, Vector3.up);
+                break;
+            case 1:
+                halfRange = level.width / 2f - badDot.transform.localScale.x;
+                position = new Vector3(Random.Range(-halfRange, halfRange), dotHeight, level.height / -2f - badDot.carryThrough);
+                rotation = Quaternion.AngleAxis(-90, Vector3.up);
+                break;
+            case 2:
+                halfRange = level.height / 2f - badDot.transform.localScale.y;
+                position = new Vector3(level.width / 2f + badDot.carryThrough, dotHeight, Random.Range(-halfRange, halfRange));
+                rotation = Quaternion.AngleAxis(180, Vector3.up);
+                break;
+            case 3:
+                halfRange = level.height / 2f - badDot.transform.localScale.y;
+                position = new Vector3(level.width / -2f - badDot.carryThrough, dotHeight, Random.Range(-halfRange, halfRange));
+                break;
+        }
+        Debug.Log(rotation);
+        BadDotController dot = Instantiate(badDot, position, rotation);
+        dot.game = this;
     }
 
     public void GameOver()
